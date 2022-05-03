ee1ce511aa7a5e21682f073303e204275020b952
Raphael Marques <raphaeljcmarques@gmail.com>
2022-05-03 17:10:17 -0300

    second class done

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function onScroll() {
   scrollY > 0 ? navigation.classList.add('scroll') : navigation.classList.remove('scroll');
 }
+function openMenu() {
+  document.body.classList.add('menu-expanded');
+}
+function closeMenu() {
+  document.body.classList.remove('menu-expanded');
+}
